e0f1b426675b6206467c23856db2108af030f04a
Román Yakimovicz <romanyaki@hotmail.com>
2026-08-15 02:19:04 -0300

    fix(backend): add missing Optional import in users.py

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, Query, HTTPException, status
 from sqlalchemy.orm import Session
